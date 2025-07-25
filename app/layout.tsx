a4ca3756f91cd35e33a97597b3d348e2411f007d
Prakhar Sahu <prakhars762@gmail.com>
2025-07-25 23:38:01 +0530

    auth

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClient } from "convex/browser";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SplitNow",
+  description: "A simple app to split bills with friends",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo.png" sizes="any" />
+      </head>
+      <body className={`${inter.className}`}>
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <Header />
+
+            <main className="min-h-screen">{children}</main>
+          </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      Prakhar Sahu
+      <br />
+      <Button variant={"destructive"}>Subscribe</Button>
+    </div>
+  );
+}
